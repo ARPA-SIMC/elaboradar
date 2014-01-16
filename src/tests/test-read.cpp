0c426fa115201261c4a84e1d1b71ac4f2f3f28ee
ezini <ezini@5e904517-b230-484e-af0d-ad602924471e>
2014-01-16 23:21:56 +0000

    Test that we read data correctly
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@1517 5e904517-b230-484e-af0d-ad602924471e

--- src/tests/test-read.cpp
+++ src/tests/test-read.cpp
@@ -15,7 +15,15 @@ void to::test<1>()
     static const char* fname = "testdata/DBP2_070120141530_GATTATICO";
     CUM_BAC* cb = new CUM_BAC;
     bool res = cb->read_sp20_volume(fname, "GAT", 0);
+    // Ensure that reading was successful
     wassert(actual(res).istrue());
+    // Ensure that nbeam_elev has been filled with the right values
+    wassert(actual(cb->nbeam_elev[0]) == 400);
+    wassert(actual(cb->nbeam_elev[1]) == 400);
+    wassert(actual(cb->nbeam_elev[2]) == 400);
+    wassert(actual(cb->nbeam_elev[3]) == 400);
+    wassert(actual(cb->nbeam_elev[4]) == 400);
+    // TODO: check some of the vol_pol contents
     delete cb;
 }
 
