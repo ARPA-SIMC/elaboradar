6c77e2e2178ad8590556d50862ab8cd953c6a606
Pier Paolo Alberoni <palberoni@arpa.emr.it>
2014-01-23 12:04:49 +0000

    Test NormalizzoData
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@1534 5e904517-b230-484e-af0d-ad602924471e

---
+++ src/tests/test-functions.cpp
@@ -0,0 +1,19 @@
+#include <wibble/tests.h>
+#include "cum_bac.h"
+
+using namespace wibble::tests;
+
+namespace tut {
+
+struct functions_shar {
+};
+TESTGRP(functions);
+
+template<> template<>
+void to::test<1>()
+{
+    // Test NormalizzoData
+    wassert(actual(NormalizzoData(0)) == 0); // TODO: check value
+}
+
+}
