bfbc4172a0dc9739e7dd65f44f79bb7e04bf87e3
palberoni <palberoni@5e904517-b230-484e-af0d-ad602924471e>
2014-03-04 11:29:13 +0000

    fixed a bug due to a missing flag
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@1789 5e904517-b230-484e-af0d-ad602924471e

--- src/cum_bac.cpp
+++ src/cum_bac.cpp
@@ -123,6 +123,19 @@ CUM_BAC::CUM_BAC(const char* site_name, bool medium)
 
     //-----  FINE INIZIALIZZAZIONI---------//
 }
+void CUM_BAC::StampoFlag(){
+    std::cout<<" Flag do_medium       :"<< (this->do_medium?" true":" false")<<std::endl;
+    std::cout<<" Flag do_clean        :"<< (this->do_clean?" true":" false")<<std::endl;  
+    std::cout<<" Flag do_quality      :"<< (this->do_quality?" true":" false")<<std::endl;
+    std::cout<<" Flag do_beamblocking :"<< (this->do_beamblocking?" true":" false")<<std::endl;
+    std::cout<<" Flag do_declutter    :"<< (this->do_declutter?" true":" false")<<std::endl;
+    std::cout<<" Flag do_bloccorr     :"<< (this->do_bloccorr?" true":" false")<<std::endl;
+    std::cout<<" Flag do_vpr          :"<< (this->do_vpr?" true":" false")<<std::endl;
+    std::cout<<" Flag do_class        :"<< (this->do_class?" true":" false")<<std::endl;
+    std::cout<<" Flag do_zlr_media    :"<< (this->do_zlr_media?" true":" false")<<std::endl;
+    std::cout<<" Flag do_devel        :"<< (this->do_devel?" true":" false")<<std::endl;
+    std::cout<<" Flag do_readStaticMap:"<< (this->do_readStaticMap?" true":" false")<<std::endl;  
+}
 
 CUM_BAC::~CUM_BAC()
 {
