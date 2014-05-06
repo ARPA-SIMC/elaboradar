ca2d92849f1aacc5699af66aec1efca8a4dfb5f8
ezini <ezini@5e904517-b230-484e-af0d-ad602924471e>
2014-05-06 15:53:00 +0000

    Updated tests to use native Eigen Matrix functions
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@1988 5e904517-b230-484e-af0d-ad602924471e

--- include/matrix.h
+++ include/matrix.h
@@ -16,21 +16,7 @@ struct Matrix2D : Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMaj
         : Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>(
                 Matrix2D::Constant(SY, SX, fill)) {}
 
-    // Number of elements in the matrix
-    size_t size() const { return this->rows() * this->cols(); }
-
     T* row(unsigned y) { return this->data() + y * this->cols(); }
-
-    T min() const { return this->minCoeff(); }
-    T max() const { return this->maxCoeff(); }
-    T avg() const
-    {
-        unsigned size = this->rows() * this->cols();
-        double mean;
-        for (unsigned i = 0; i < size; ++i)
-            mean += (double)this->data()[i] / size;
-        return round(mean);
-    }
 };
 
 }
