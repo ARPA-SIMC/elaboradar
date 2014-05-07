e99c89838f97601a20266c4c2b5832ca4fe06659
Pier Paolo Alberoni <palberoni@arpa.emr.it>
2014-05-07 12:59:15 +0000

    Deal with a bug in log4c that doesn't want to be initialized twice
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@1992 5e904517-b230-484e-af0d-ad602924471e

--- src/logging.cpp
+++ src/logging.cpp
@@ -4,15 +4,30 @@
 
 using namespace std;
 
+namespace {
+static bool initialized = false;
+}
+
+/*
+ * log4c is currently causing "double free or corruption" if log4c_init is
+ * called twice (even with a log4c_fini inbetween).
+ *
+ * Working around this by calling init just once, and never calling fini
+ */
+
 Logging::Logging()
 {
-    // Initialize logging
-    if (log4c_init() != 0)
-        throw runtime_error("failed to set up logging");
+    if (!initialized)
+    {
+        // Initialize logging
+        if (log4c_init() != 0)
+            throw runtime_error("failed to set up logging");
+        initialized = true;
+    }
 }
 
 Logging::~Logging()
 {
-    if (log4c_fini() != 0)
-      fprintf(stderr, "failed to shut down logging.\n");
+    // if (log4c_fini() != 0)
+    //   fprintf(stderr, "failed to shut down logging.\n");
 }
