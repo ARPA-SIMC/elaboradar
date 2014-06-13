5451c4c92621db015230db84b941b7017c33afe4
Pier Paolo Alberoni <palberoni@arpa.emr.it>
2014-06-13 10:34:47 +0000

    implementati in matrix.h metodi log10 ed exp10 che operano efficientemente coefficientwise è ora possibile anche implemntare facilmente metodi expX ed logX al bisogno
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@2087 5e904517-b230-484e-af0d-ad602924471e

--- include/matrix.h
+++ include/matrix.h
@@ -5,7 +5,27 @@
 #include <cmath>
 #include <Eigen/Dense>
 
+#include <iostream>
+
 namespace cumbac {
+
+template<typename Scalar> struct Exponential {
+EIGEN_EMPTY_STRUCT_CTOR(Exponential)
+//typedef Scalar result_type;
+Scalar operator()(const Scalar& base, const Scalar& exponent) const { return std::pow(base,exponent); }
+};
+
+template<typename Scalar> struct Logarithm {
+EIGEN_EMPTY_STRUCT_CTOR(Logarithm)
+//typedef Scalar result_type;
+Scalar operator()(const Scalar& base, const Scalar& argument) const { return std::log(argument)/std::log(base); }
+};
+
+template<typename Scalar> struct Log10 {
+EIGEN_EMPTY_STRUCT_CTOR(Log10)
+//typedef Scalar result_type;
+Scalar operator()(const Scalar& argument) const { return std::log10(argument); }
+};
 
 /// Base for all matrices we use, since we rely on row-major data
 template<typename T>
@@ -14,6 +34,18 @@ struct Matrix2D : public Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen:
     using Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>::Matrix;
 
     T* row_ptr(unsigned y) { return this->data() + y * this->cols(); }
+
+    Matrix2D<T> log10()
+    {
+	return this->unaryExpr(Log10<T>());
+    }
+    Matrix2D<T> exp10()
+    {
+	Matrix2D<T> base(Matrix2D<T>::Constant(this->rows(),this->cols(),10.));
+	return base.binaryExpr(*this,Exponential<T>());
+    }
+
+
 };
 
 template<typename T>
