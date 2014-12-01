251db54a43bcbe720e6cd7b023b8eac8a291c578
Enrico Zini <enrico@enricozini.org>
2014-12-01 11:35:07 +0000

    Removed unused constants
    git-svn-id: https://svn.smr.arpa.emr.it/radar/trunk@2339 5e904517-b230-484e-af0d-ad602924471e

--- include/par_class.h
+++ include/par_class.h
@@ -3,8 +3,4 @@
 #define STEINER_RADIUS 11.
 #define h_radar 0.02 
 #define CONV_VAL 100
-#define THRES_ALLERT1 40.
-#define THRES_ALLERT2 50.
-#define N_PUNTI_MIN 4 
-#define  THR_VIZ 3000.
-
+#define THR_VIZ 3000.
